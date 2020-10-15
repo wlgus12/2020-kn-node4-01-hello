23d2ee018fe06d4072bad9accf82532378c46864
wlgus12 <wlgus4538@naver.com>
2020-10-15 22:21:43 +0900

    commit

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.listen(3000, function(){
     console.log("http://127.0.0.1:3000");
 });
 
-app.get('/', function(req,res,next){
-    res.send('<h1>Hello Word</h1>')
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+//console.log(path.join(__dirname,'./public'));
+app.use('/',express.static(path.join(__dirname,'./public')));
+
+
+app.get('/hello', function(req,res,next){
+    res.send('<html><head><title>Hi~</title></head><body><h1>Hello World</h1></body></html>')
 });
-app.get('/wlgus12', function(req,res,next){
+/* app.get('/wlgus12', function(req,res,next){
     res.send('<h1>Hello jenny</h1>')
+}); */
+
+app.get('/sample', function(req,res,next){
+    var title = '샘플페이지'; 
+    var content = 'Hello샘플' ;
+    var html = `
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${title}</title>
+    </head>
+    <body>
+        <h1>${content}</h1>
+    </body>
+    </html>
+    `;
+    res.send(html);
+});
+
+
+
+app.get('/search', function(req,res){
+    var q = req.query.q;
+    res.send(`<h1>당신이 요청한 쿼리는 ${q}입니다.</h1>`);
+});
+
+app.get('/user/:id', function(req,res){
+    var id = req.params.id;
+    /* request = 요청하다 params라는 변수에서 아이디를 찾아라 */
+    res.send(`<h1>안녕하세요 ${id}님.</h1>`);
+});
+
+app.post('/join', function(req,res){
+    var userid = req.body.userid;
+    var userpw = req.body.userpw;
+    res.send(`<h1>${userid} / ${userpw}</hl>`)
 });
--- readme.md
+++ readme.md
@@ -1,15 +1,25 @@
 # 2020.10.13 노드수업
 ## 1. Visual Studio 다운로드 및 설치
-## 2.nodejs 설치
+## 2. nodejs 설치
 - [노드 사이트](https://nodejs.org)
-## 3.git 설치
+## 3. git 설치
 - [git-scm.com](https://git-scm.com)
-## 4.git 가입
-## 5.본인의 컴퓨터에 git 사용자 등록
-- com창을 열고
+## 4. github 가입
+## 5. 본인의 컴퓨터에 git 사용자 등록(한번만)
+- cmd창을 열고
 ```bash
-git config --global user.name 'wlgus12'
-git config --global user.email 'wlgus4538@naver.com'
-``` 
+git config --global user.name 'booldook'
+git config --global user.email 'booldook@gmail.com'
+```
+## 6. 프로젝트 폴더를 만들고 vscode에서 연 다음, git을 설정한다.
+```bash
+git init
+git remote add origin https://github.com/wlgus12/2020-kn-node4-01-hello.git
+```
+## 7. 터미널 창에서 다음과 같이 node프로젝트를 시작한다.
+```bash
+npm init -y
+```
+## 8. root에 package.json 파일이 생성되면 프로젝트 생성이 완료됨.
 
 
